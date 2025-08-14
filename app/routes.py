18984580a4a0c3c8bc11a1f95399f109c76722c8
LEONARDO DI FRANCO <81536164+difranardo@users.noreply.github.com>
2025-08-14 14:34:30 -0300

    sda

--- app/routes.py
+++ app/routes.py
@@ -1,9 +1,8 @@
 from flask import Flask
 from flask_login import current_user
-from app.auth.application.login_google import auth_bp, init_auth
 from app.scraper.application.scraper_routes import scraper_bp
 
-@scraper_bp.before_request
+#@scraper_bp.before_request
 def must_be_logged():
     # Chequea autenticación
     if not current_user.is_authenticated:
@@ -16,7 +15,5 @@ def set_routes(app: Flask) -> None:
         return "CAT OK"
 
     # Auth routes
-    init_auth(app)
-    app.register_blueprint(auth_bp)
     # Cat routes
     app.register_blueprint(scraper_bp)
